763241f28b7f6548e76dfa8a3002f67fdafeec5f
Stewart ilondanga <stilondanga@gmail.com>
2021-04-05 14:54:52 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,12 @@ $(document).ready(function() {
         var pageNum = i + 1;
         $('#nav').append('<a href="#" rel="' + i + '">' + pageNum + '</a> ');
       }
-      /*$('#data tbody tr').hide();
-    $('#data tbody tr').slice(0, rowsShown).show();
-    $('#nav a:first').addClass('active');
-    $('#nav a').bind('click', function(){
+      $('#data tbody tr').hide();
+      $('#data tbody tr').slice(0, rowsShown).show();
+      $('#nav a:first').addClass('active');
+      $('#nav a').bind('click', function() {
 
-        $('#nav a').removeClass('active');
+            /*$('#nav a').removeClass('active');
         $(this).addClass('active');
         var currPage = $(this).attr('rel');
         var startItem = currPage * rowsShown;
